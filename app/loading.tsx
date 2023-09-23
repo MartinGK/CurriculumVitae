caa5a734971004b6a217db8617446f04dbd06b57
MartinGK <mrtgeka@gmail.com>
2023-09-23 17:39:42 -0300

    fix: lint

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,9 @@
 import Loader from "./components/Loader";
 
 export default function Loading() {
-    return (
-        <div className="flex justify-center items-center w-full h-full">
-            <Loader />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <Loader />
+    </div>
+  );
 }
