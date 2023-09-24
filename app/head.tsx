8cf41d512561bf9275a3273a4c4957c71db036c8
MartinGK <mrtgeka@gmail.com>
2023-09-24 15:14:43 -0300

    feat: pwa

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata & { google: string } = {
+  title: "Martin GK",
+  description: "Martin Gainza Koulaksezian web page CV",
   colorScheme: "dark",
   google: "notranslate",
   manifest: "/manifest.json",
