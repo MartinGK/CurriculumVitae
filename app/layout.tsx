0bd9b14909542d042ce35bbc375d5e55037e6b75
MartinGK <mrtgeka@gmail.com>
2023-09-25 13:10:52 -0300

    fix: metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,30 @@ import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Martin GK",
-  description: "Martin GK online cv"
+  description: "Martin GK online cv",
+  themeColor: "#000", 
+  icons: [
+    {
+      url: "/images/icon-192x192.png",
+      sizes: "192x192",
+      type: "image/png"
+    },
+    {
+      url: "/images/icon-256x256.png",
+      sizes: "256x256",
+      type: "image/png"
+    },
+    {
+      url: "/images/icon-384x384.png",
+      sizes: "384x384",
+      type: "image/png"
+    },
+    {
+      url: "/images/icon-512x512.png",
+      sizes: "512x512",
+      type: "image/png"
+    }
+  ]
 };
 
 export default function Main({ children }: { children: ReactNode }) {
