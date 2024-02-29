ec4d4c7210539147c09ab069cbdd1546dca939f3
MartinGK <mrtgeka@gmail.com>
2024-02-28 23:19:43 -0300

    redirect added

--- pages/books/word-search-1/index.tsx
+++ pages/books/word-search-1/index.tsx
@@ -1,8 +1,6 @@
+import { useRouter } from "next/navigation";
 
-export default function index() {
-  return (
-    <div>
-        Testing
-    </div>
-  );
+export default function WordBook1() {
+  const router = useRouter();
+  router.push("https://www.amazon.com/review/create-review/ref=cm_cr_othr_d_wr_but_top?ie=UTF8&channel=glance-detail&asin=B0CWPMCJJC");
 }
